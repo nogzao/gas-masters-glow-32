71e3f85a78994fefe6293becc3d847a52330879f
nog <123551457+nogzao@users.noreply.github.com>
2025-11-07 17:24:53 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => {
             {/* Ele está fora das Rotas, por isso aparecerá em todas as páginas */}
             <iframe 
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.2988738214967!2d-43.291756024689754!3d-22.902344579258134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997dc6ae4b27b1%3A0x7b121a6cafe9463a!2sAUTORIZADA%20RINNAI!5e0!3m2!1spt-BR!2sbr!4v1762537430442!5m2!1spt-BR!2sbr" 
-              width="600" 
+              /*width="600"*/
               height="450" 
               style={{ border: 0 }} 
               allowFullScreen="" 
